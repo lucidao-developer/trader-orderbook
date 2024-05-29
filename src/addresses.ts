521775aaf2d9ee5113d05150637253c5e9437536
Drikkx <88060062+Drikkx@users.noreply.github.com>
2024-05-29 12:01:46 +0200

    update marketplace

--- src/addresses.ts
+++ src/addresses.ts
@@ -9,7 +9,7 @@ const addresses: { [key: string]: AddressesForChain | undefined } = {
     wrappedNativeToken: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
   },
   '974399131': {
-    exchange: '0x33e6A26A94d6165Ac32F29cB291dEFdfE75c4a1d',
+    exchange: '0x09AF4dd7eCdC3c75Ed9032F8ed34bC2218c70DFF',
     wrappedNativeToken: '0xc778417e063141139fce010982780140aa0cd5ab',
   },
 }
