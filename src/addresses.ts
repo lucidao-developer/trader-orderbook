b76df02eb685d3e1fed563cd30672875850a95ce
raoulpicconi <raoulpicconi03@gmail.com>
2024-06-06 19:32:55 +0200

    chore: new zeroEx exchange proxy address

--- src/addresses.ts
+++ src/addresses.ts
@@ -5,7 +5,7 @@ export interface AddressesForChain {
 
 const addresses: { [key: string]: AddressesForChain | undefined } = {
   '80002': {
-    exchange: '0xc6ed17457a675cdcb38077a427ffe904728d6a45',
+    exchange: '0x57F415CC50706d4862495ABEDF4b90b6E9217d62',
     wrappedNativeToken: '0x01805a841ece00cf680996bf4b4e21746c68fd4e',
   }
 }
